fcf2fca6a1ec30d61076d1416ef87c66e82dcadf
Michael Campbell <michael.campbell@gmail.com>
2024-11-03 21:01:28 -0500

    Changed test to use better asssertions

--- monthly_challenges/challenges/tests/test_integration_challenges_month.py
+++ monthly_challenges/challenges/tests/test_integration_challenges_month.py
@@ -1,12 +1,13 @@
+from playwright.sync_api import Page, expect
+
 from challenges.views import FEB_OUTPUT, JAN_OUTPUT
-from playwright.sync_api import Page
 
 
 def test_jan(page: Page):
     page.goto("http://localhost:8000/challenges/january")
-    assert page.content().count(JAN_OUTPUT) > 0
+    expect(page.locator("css=body")).to_contain_text(JAN_OUTPUT)
 
 
 def test_feb(page: Page):
     page.goto("http://localhost:8000/challenges/february")
-    assert page.content().count(FEB_OUTPUT) > 0
+    expect(page.locator("css=body")).to_contain_text(FEB_OUTPUT)
