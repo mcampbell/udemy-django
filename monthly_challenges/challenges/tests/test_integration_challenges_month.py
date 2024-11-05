f233e4fdcd1c287e7afa364781c217e5a2d4842f
Michael Campbell <michael.campbell@gmail.com>
2024-11-04 21:11:33 -0500

    simplified playwright test locator

--- monthly_challenges/challenges/tests/test_integration_challenges_month.py
+++ monthly_challenges/challenges/tests/test_integration_challenges_month.py
@@ -4,10 +4,10 @@ from playwright.sync_api import Page, expect
 def test_any_month_with_string(page: Page):
     for month in ["january", "february"]:
         page.goto(f"http://localhost:8000/challenges/{month}")
-        expect(page.locator("css=body")).to_contain_text(f"My goal for {month}.")
+        expect(page.locator("body")).to_contain_text(f"My goal for {month}.")
 
 
 def test_any_month_with_int(page: Page):
     for month in range(2):
         page.goto(f"http://localhost:8000/challenges/{month}")
-        expect(page.locator("css=body")).to_contain_text(f"My goal for the month number {month}.")
+        expect(page.locator("body")).to_contain_text(f"My goal for the month number {month}.")
