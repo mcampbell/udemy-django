29b2fd8ace76b2f095fe1ccacf9dac14cd756ada
Michael Campbell <michael.campbell@gmail.com>
2024-11-06 20:09:12 -0500

    Played with HttpResponseRedirect
    = modified the challenges a bit.

--- monthly_challenges/challenges/tests/test_integration_challenges_month.py
+++ monthly_challenges/challenges/tests/test_integration_challenges_month.py
@@ -1,13 +1,23 @@
 from playwright.sync_api import Page, expect
 
 
-def test_any_month_with_string(page: Page):
+def test_any_month_with_string(page: Page) -> None:
     for month in ["january", "february"]:
         page.goto(f"http://localhost:8000/challenges/{month}")
-        expect(page.locator("body")).to_contain_text(f"My goal for {month}.")
+        expect(page.locator("body")).to_contain_text(f"Your challenge for {month.title()}")
 
 
-def test_any_month_with_int(page: Page):
-    for month in range(2):
-        page.goto(f"http://localhost:8000/challenges/{month}")
-        expect(page.locator("body")).to_contain_text(f"My goal for the month number {month}.")
+def test_bad_month(page: Page) -> None:
+    page.goto(f"http://localhost:8000/challenges/Nope")
+    expect(page.locator("body")).to_contain_text(f"Invalid month")
+
+
+def test_any_month_with_int(page: Page) -> None:
+    page.goto(f"http://localhost:8000/challenges/1")
+    expect(page.locator("body")).to_contain_text(f"Your challenge for January")
+
+    page.goto(f"http://localhost:8000/challenges/3")
+    expect(page.locator("body")).to_contain_text(f"Your challenge for March")
+
+    page.goto(f"http://localhost:8000/challenges/13")
+    expect(page.locator("body")).to_contain_text(f"Invalid month")
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,10 +1,32 @@
-from django.http import HttpResponse, HttpRequest
+from configparser import Error
+
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+
+month_names = [
+    "january",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december",
+]
+challenges = dict([(month, f"Your challenge for {month.title()}") for month in month_names])
+
 
 
 # Create your views here.
 def monthly_goal(request: HttpRequest, month: str) -> HttpResponse:
-    return HttpResponse(f"My goal for {month}.")
-
+    try:
+        return HttpResponse(challenges[month])
+    except Error:
+        return HttpResponse("Invalid month.")
 
 def monthly_goal_by_cardinal(request: HttpRequest, month: int) -> HttpResponse:
-    return HttpResponse(f"My goal for the month number {month}.")
+    # return monthly_goal(request, month_names[month - 1])
+    return HttpResponseRedirect(f"/challenges/{month_names[month - 1]}")
