2d0379ddabcc4d7a90e414e3e4ddba1149e7577b
Michael Campbell <michael.campbell@gmail.com>
2024-11-11 21:22:20 -0500

    added nav snippet/include

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="root-monthly-challenge"),
+    path("", views.index, name="index"),
     path("<int:month>", views.monthly_goal_by_cardinal),
     path("<str:month>", views.monthly_goal, name="string-monthly-challenge"),
 ]
