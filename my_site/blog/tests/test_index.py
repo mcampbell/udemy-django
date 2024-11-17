9a25535e6bdb143fb4297e8dc9435af6b5fd4962
Michael Campbell <michael.campbell@gmail.com>
2024-11-17 11:29:55 -0500

    Ensure both /blog and / go to the blog app

--- my_site/blog/tests/test_index.py
+++ my_site/blog/tests/test_index.py
@@ -4,3 +4,7 @@ from playwright.sync_api import Page, expect
 def test_index(page: Page):
     page.goto("http://localhost:8000/blog")
     expect(page.locator("text=Welcome to the blog!")).to_be_visible()
+
+def test_root_goes_to_blog(page:Page) -> None:
+    page.goto("http://localhost:8000/")
+    expect(page.locator("text=Welcome to the blog!")).to_be_visible()
--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("blog.urls")),
     path("blog/", include("blog.urls")),
     path("admin/", admin.site.urls),
 ]
