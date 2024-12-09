89081b012f6d1b727973eeb82d057b8d3192dea6
Michael Campbell <michael.campbell@gmail.com>
2024-12-08 20:13:49 -0500

    Added single post functionality
    + 404 functionality (can't see this in debug mode)

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -73,19 +73,21 @@ posts = [
 
 
 def get_date(post):
-    return post['date']
+    return post["date"]
 
 
 # Create your views here.
 def welcome(request: HttpRequest) -> HttpResponse:
     sorted_posts = sorted(posts, key=get_date, reverse=True)
-    latest_posts = sorted_posts[-3:] # last 3 posts, since we sorted by date ascending.
+    latest_posts = sorted_posts[-3:]  # last 3 posts, since we sorted by date ascending.
     return render(request, "blog/index.html", {"posts": latest_posts})
 
 
 def post_list(request: HttpRequest) -> HttpResponse:
-    return render(request, "blog/all-posts.html")
+    sorted_posts = sorted(posts, key=get_date, reverse=True)
+    return render(request, "blog/all-posts.html", {"post_list": sorted_posts})
 
 
 def post_detail(request: HttpRequest, slug: str) -> HttpResponse:
-    return render(request, "blog/post-detail.html")
+    post = next(post for post in posts if post["slug"] == slug)
+    return render(request, "blog/post-detail.html", {"post": post})
