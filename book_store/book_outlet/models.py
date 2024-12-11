cfb61ab8216a7f2f1ac89cd4415a57a57e86882f
Michael Campbell <michael.campbell@gmail.com>
2024-12-11 13:35:44 -0500

    Updated the model.
    + author
    + is_bestseller
    + validators
    + default values
    + __str__() method

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[
+        MinValueValidator(1),
+        MaxValueValidator(5)
+    ])
+    author = models.CharField(max_length=100, default="Unknown")
+    is_bestseller = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
