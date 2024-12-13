1485529f584dfe876b524533ea5835d4e9e84cdc
Michael Campbell <michael.campbell@gmail.com>
2024-12-13 13:28:21 -0500

    Started database work
    + get books from DB for all-books page
    + template for all-books
    + template for single book
    + wire up code to get all-books and single book to template
    + link all-books entry to single book page

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,2 +1,21 @@
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Book
+
+
+def index(request):
+    def by_title(book):
+        return book.title
+
+    all_books = sorted(Book.objects.all(), key=lambda book: book.title)
+    return render(request, "book_outlet/index.html", {"books": all_books})
+
+
+def book_detail(request, id):
+    # try:
+    #     book = Book.objects.get(pk=id)
+    # except:
+    #     raise Http404("Book not found")
+
+    book = get_object_or_404(Book, pk=id)
+    return render(request, "book_outlet/book_detail.html", {"book": book})
