27877e55606c4d57869937648ea18c21e4913f47
Michael Campbell <michael.campbell@gmail.com>
2024-12-17 13:00:15 -0500

    added some admin stuff
    + registered Book model in admin
    = made slug readonly in admin
    = made display use title and author only

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,2 +1,12 @@
 
-# Register your models here.
+from django.contrib import admin
+
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    readonly_fields = ("slug",)
+    list_display = ("title", "author")
+
+
+admin.site.register(Book, BookAdmin)
+
