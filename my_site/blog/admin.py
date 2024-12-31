474ea15fce564dc2bf028cc33c3b00a4ab1e939e
Michael Campbell <michael.campbell@gmail.com>
2024-12-31 15:43:15 -0500

    Created new models for blog.
    + Post
    + Author (1-M to Post)
    + Tag (M-M to Post)
    + Registered default configs for each in admin
    
    No migrations run yet.

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,1 +1,8 @@
 # Register your models here.
+from django.contrib import admin
+
+from .models import Author, Tag, Post
+
+admin.site.register(Author)
+admin.site.register(Tag)
+admin.site.register(Post)
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,1 +1,24 @@
-# Create your models here.
+from django.db import models
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=150, null=True)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField()
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=50)
+
+
+class Post(models.Model):
+    # Normally this'd be a URL, but for simplicity, we're just using the image name which we'll assume to be in the
+    # static folder
+    image = models.CharField(max_length=500)
+    slug = models.SlugField(max_length=150, unique=True, db_index=True)
+    date = models.DateField(auto_now=True)
+    title = models.CharField(max_length=150)
+    excerpt = models.TextField()  # could just be a longer CharField
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="posts")
+    tags = models.ManyToManyField(Tag, related_name="posts")
