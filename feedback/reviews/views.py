5d32ff0aebe76fdb9107fd8346c5aec5ca23870f
Michael Campbell <michael.campbell@gmail.com>
2025-01-05 20:46:50 -0500

    Added a thank-you page.
    = POST activity now does a thing, however small, and redirects user to
    a thank you page.

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def review(request):
-    return render(request, "reviews/review.html")
+    # We have both a post (submits a review) and a get (displays the review form) request.
+    if request.method == "POST":
+        # We will need to process the form data.
+        username = request.POST["username"]
+        return HttpResponseRedirect("/thank-you")
+
+    else:
+        return render(request, "reviews/review.html")
+
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
