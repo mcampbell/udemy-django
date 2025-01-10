77a71b15e1cb40a0210a1fcdaaf6e767acf1ebd2
Michael Campbell <michael.campbell@gmail.com>
2025-01-10 16:50:10 -0500

    added review text and rating to form
    + review text, rating in form with appropriate builtin validators
    = view to show new form elements.  learned you can `for` iterate over
      fields in a form

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -4,7 +4,21 @@ from django import forms
 class ReviewForm(forms.Form):
     username = forms.CharField(
         label="Your Name",
-        max_length=100,
         min_length=2,
+        max_length=100,
         error_messages={"required": "Username is required."},
+    )
+
+    review = forms.CharField(
+        label="Your Feedback",
+        widget=forms.Textarea,
+        max_length=200,
+        error_messages={"required": "Review is required."},
+    )
+
+    rating = forms.IntegerField(
+        label="Your Rating",
+        min_value=1,
+        max_value=5,
+        error_messages={"required": "Rating is required."},
     )
