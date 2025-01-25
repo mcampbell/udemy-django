968d505378bf461b1f5b0fe3d733e70b3df88fad
Michael Campbell <michael.campbell@gmail.com>
2025-01-25 18:19:56 -0500

    Converted the module.function view function to Class based view

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,13 +1,18 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse, HttpRequest
 from django.shortcuts import render
+from django.views import View
 
 from .forms import ReviewForm
-from .models import Review
 
 
-def review(request):
-    # We have both a post (submits a review) and a get (displays the review form) request.
-    if request.method == "POST":
+class ReviewView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        # create a new empty form to show in the case of a GET. We need to use the same variable name as the POST
+        # request so that can just render WHATEVER form happens to get picked based on HTTP method.
+        form = ReviewForm()
+        return render(request, "reviews/review.html", {"form": form})
+
+    def post(self, request: HttpRequest) -> HttpResponse:
         form = ReviewForm(request.POST)
         if form.is_valid():
             # This is a ModelForm so can save directly.
@@ -16,12 +21,8 @@ def review(request):
             # everything's cool, move ahead.
             return HttpResponseRedirect("/thank-you")
 
-    else:
-        # create a new empty form to show in the case of a GET. We need to use the same variable name as the POST
-        # request so that can just render WHATEVER form happens to get picked based on HTTP method.
-        form = ReviewForm()
-
-    return render(request, "reviews/review.html", {"form": form})
+        else:
+            return render(request, "reviews/review.html", {"form": form})
 
 
 def thank_you(request):
