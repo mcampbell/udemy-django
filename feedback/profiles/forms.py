c0b6f122337c71b5042b668ba4f2db16b9aeeaeb
Michael Campbell <michael.campbell@gmail.com>
2025-02-01 18:07:54 -0500

    Changed upload form to a Form helper

---
+++ feedback/profiles/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class ProfileForm(forms.Form):
+    # This is taking the place of the form in the template, which for us right now is ONLY the file upload input.
+    user_image = forms.FileField()
--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,14 +1,23 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from .forms import ProfileForm
 
 # Create your views here.
 
 
+def store_file(file):
+    with open("/tmp/" + file.name, "wb+") as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+
+
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {"form": form})
 
     def post(self, request):
-        print(request.FILES["image"])
+        print(request.FILES["user_image"])
+        store_file(request.FILES["user_image"])
         return HttpResponseRedirect("/profiles")
