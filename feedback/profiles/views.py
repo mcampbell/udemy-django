31f0803e22352896b8d4ef071f8985b2a22355bc
Michael Campbell <michael.campbell@gmail.com>
2025-02-02 13:51:16 -0500

    Started a FileForm in a model handle file uploads.

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,23 +1,23 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+
 from .forms import ProfileForm
+from .models import UserProfile
+
 
 # Create your views here.
 
 
-def store_file(file):
-    with open("/tmp/" + file.name, "wb+") as destination:
-        for chunk in file.chunks():
-            destination.write(chunk)
-
-
 class CreateProfileView(View):
     def get(self, request):
-        form = ProfileForm()
+        form = ProfileForm()  # for rendering
         return render(request, "profiles/create_profile.html", {"form": form})
 
     def post(self, request):
-        print(request.FILES["user_image"])
-        store_file(request.FILES["user_image"])
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
+
         return HttpResponseRedirect("/profiles")
