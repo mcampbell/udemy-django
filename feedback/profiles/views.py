541ec432381b3ebc7aa30d480a41b99cdbfd5340
Michael Campbell <michael.campbell@gmail.com>
2025-02-10 17:38:16 -0500

    Changed to a "CreateView"
    - got rid of the form helper since CreateView incorporates the
    "ViewModel" idea and sucks in the model and creates the form from the
    model attributes.

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,23 +1,13 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.views import View
+from django.views.generic.edit import CreateView
 
-from .forms import ProfileForm
 from .models import UserProfile
 
 
 # Create your views here.
 
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()  # for rendering
-        return render(request, "profiles/create_profile.html", {"form": form})
-
-    def post(self, request):
-        form = ProfileForm(request.POST, request.FILES)
-        if form.is_valid():
-            profile = UserProfile(image=request.FILES["user_image"])
-            profile.save()
-
-        return HttpResponseRedirect("/profiles")
+class CreateProfileView(CreateView):
+    model = UserProfile
+    fields = "__all__"
+    template_name = "profiles/create_profile.html"
+    success_url = "/profiles"
