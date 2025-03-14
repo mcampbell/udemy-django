56e9586b6ab1cc89f98217208a0cca6a8c2b837f
Michael Campbell <michael.campbell@gmail.com>
2025-03-14 08:47:48 -0400

    Config settings to allow uploaded images to be seen/displayed

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,4 +24,4 @@ urlpatterns = [
     path("", include("blog.urls")),
     path("blog/", include("blog.urls")),
     path("admin/", admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # map the bespoke media URL to the media root
